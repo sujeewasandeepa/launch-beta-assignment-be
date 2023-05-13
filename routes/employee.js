b37f51d7416d43f9a7f8ca9acf74652d60731275
L.A.S.S. Kularathna <sujeewasandeepa@protonmail.ch>
2023-05-13 10:40:37 +0530

    Add controllers to the routes

--- routes/employee.js
+++ routes/employee.js
@@ -2,18 +2,10 @@ const express = require('express');
 const employeeRouter = express.Router();
 const EmployeeController = require('../controllers/EmployeeController');
 
-employeeRouter.post("/add", (req, res) => {
-    res.send("Add employee");
-});
-
-employeeRouter.delete("/delete", (req, res) => {
-    res.send("Delete employee");
-});
-
+employeeRouter.post("/add", EmployeeController.addEmployee);
+employeeRouter.delete("/delete", EmployeeController.deleteEmployee);
 employeeRouter.get("/", EmployeeController.getEmployees);
-
-employeeRouter.put("/edit", (req, res) => {
-    res.send("Update employee");
-});
+employeeRouter.put("/edit", EmployeeController.editEmployee);
+employeeRouter.get("/filter", EmployeeController.filterEmployees);
 
 module.exports = employeeRouter;
