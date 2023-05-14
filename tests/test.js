82ae52fe0fd91b6dc4df6105cf359cdf1ef0a7b8
Sujeewa Sandeepa <sujeewasandeepa@protonmail.ch>
2023-05-14 11:16:18 +0530

    tried to use mocha and chai for testing

---
+++ tests/test.js
@@ -0,0 +1,11 @@
+// const { expect } = require('chai');
+// const EmployeeController = "../controllers/EmployeeController.js";
+
+// describe("EmployeeController", () => {
+//     describe("getEmployees", () => {
+//         it('should get all the employees in the database', () => {
+//             const result = EmployeeController.getEmployees();
+//             expect(result).to.equal();
+//         })
+//     })
+// })
